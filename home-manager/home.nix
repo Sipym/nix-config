b2ec494b9bb6fd1d87579bc4b19cd6ac80efc3fe
PangYeming <1983156733@qq.com>
2025-09-30 14:16:40 +0800

    remove bear

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,7 +55,6 @@
     pkgs.circt        # chisel相关; 在flake.nix中通过overlay指定了版本1.62.0
     pkgs.jdk24        # java环境
     pkgs.metals
-    pkgs.bear
   ];
 
   #===================================================
